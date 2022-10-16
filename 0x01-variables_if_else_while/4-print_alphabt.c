63afe5a38190386db6ecd4185c95f21030eb3eb0
mwaskagi <gituiku.mwangi@gmail.com>
2022-10-16 09:50:27 -0700

    Skip Q and E

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet expect q and e
+ * descritpion: will use putchar to list the alphabet without q and e in lowercase
+ * Return: 0
+ */
+
+int main(void)
+{
+	int ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' || ch != 'q')
+			putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
