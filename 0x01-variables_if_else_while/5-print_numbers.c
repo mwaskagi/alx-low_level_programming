33de89ef38c349836552f30ce96fa14dc9665016
mwaskagi <gituiku.mwangi@gmail.com>
2022-10-16 10:38:57 -0700

    print digitd

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digits
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
