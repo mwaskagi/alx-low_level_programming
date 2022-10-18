1ee7ed080a72a9ec3eaf05e3f88d97dd612d192b
mwaskagi <gituiku.mwangi@gmail.com>
2022-10-18 13:00:15 -0700

    is Lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - check is charcter is lowe
+ *
+ * Return: 1 if true else 0 
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
