c254aab6621fe8c8571a565eba05ef96c084c486
mwaskagi <gituiku.mwangi@gmail.com>
2022-10-18 12:47:29 -0700

    loop 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,12 +13,10 @@ void print_alphabet_x10(void)
 
 	while (i++ <= 9)
 	{
-
-           for (ch = 'a'; ch <= 'z'; ch++)
-           {  
-		_putchar(ch);
-           }   
-	
-	   _putchar('\n');
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
 	}
 }
