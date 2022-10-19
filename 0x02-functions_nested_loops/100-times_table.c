747fb5780f08422c94243483f461d518164feb90
mwaskagi <gituiku.mwangi@gmail.com>
2022-10-19 10:16:07 -0700

    time table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,13 +14,17 @@ void print_times_table(int n)
 		for (first = 0; first <= n; first++)
 		{
 			_putchar('0');
-
+		
 			for (last = 1; last <= n; last++)
 			{
-				ret = first * last;
-				_putchar(ret + '0');
 				_putchar(',');
 				_putchar(' ');
+				ret = first * last;
+				if (ret <= 9)
+					_putchar(' ');
+				else
+					_putchar((ret / 10) + '0');
+				_putchar((ret % 10) + '0');
 			}
 			_putchar('\n');
 		}
