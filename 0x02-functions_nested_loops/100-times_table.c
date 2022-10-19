ea7c881931ac6745341a714711edd34bb7709a9d
mwaskagi <gituiku.mwangi@gmail.com>
2022-10-19 10:26:50 -0700

    time table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -28,8 +28,8 @@ void print_times_table(int n)
 				{
 					_putchar((ret / 10) + '0');
 				}
-				_putchar(' ');
 				_putchar((ret % 10) + '0');
+				_putchar(' ');
 			}
 			_putchar('\n');
 		}
