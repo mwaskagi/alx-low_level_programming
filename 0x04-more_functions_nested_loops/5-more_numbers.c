292480159ff5098ab731cd67414247d184deff3e
mwaskagi <gituiku.mwangi@gmail.com>
2022-10-21 11:47:13 -0700

    Loop of 10

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,7 +15,9 @@ void more_numbers(void)
 
 		for (y = 0; y <= 14; y++)
 		{
-			_putchar(y + '0');
+			if (i >= 10)
+				_putchar((y / 10) + '0');
+			_putchar((y % 10) + '0');
 		}
 		_putchar('\n');
 	}
