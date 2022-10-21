cb4b16ce29b8ae1c29b5ac9b4f9f14af5d836634
mwaskagi <gituiku.mwangi@gmail.com>
2022-10-21 09:40:16 -0700

    Is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper : checkes if charcter is in upper
+ * Return: 1 if true and 0 if false
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
