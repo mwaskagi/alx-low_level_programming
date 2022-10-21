ea2c82a1fe2a306c1c8bab3587b8713e947210b0
mwaskagi <gituiku.mwangi@gmail.com>
2022-10-21 13:28:15 -0700

    Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - print buzz function
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if ((x % 3 == 0) && (x % 5 == 0))
+		{
+			printf(" FizzBuzz");
+		}
+		else if (x % 3 == 0)
+		{
+			printf(" Fuzz");
+		}
+		else if (x % 5 == 0)
+		{
+			printf(" Buzz");
+		}
+		else
+		{
+			printf(" %d", x);
+		}
+	}
+	printf("\n");
+	return (0);
+}
