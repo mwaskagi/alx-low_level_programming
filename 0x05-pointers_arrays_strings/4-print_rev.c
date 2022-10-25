3daadc54e0869a061ba35fd0bc40394ee2dcd157
mwaskagi <gituiku.mwangi@gmail.com>
2022-10-25 12:34:14 -0700

    Print in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - Print string in reverse
+ * @s: contain the string
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+	int count, checker;
+
+	count = 0;
+	while (s[count] != s['\0'])
+	{
+		count++;
+	}
+
+	checker = 0;
+	while (checker == count)
+	{
+		_putchar(s[counter]);
+		count--;
+	}
+	_putchar('\n');
+}
