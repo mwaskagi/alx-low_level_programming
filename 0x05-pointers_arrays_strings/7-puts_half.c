23b221de03c26546504066b24be7573f989f557d
mwaskagi <gituiku.mwangi@gmail.com>
2022-10-25 13:33:12 -0700

    Print half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of the srting
+ * @str: contain string
+ * Return: 0
+ */
+
+void puts_half(char *str)
+{
+	int i, n;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+
+	if (i % 2 != 0)
+	{
+		n = (i - 1) / 2;
+		while (str[n] != '\0')
+		{
+			_putchar(str[n]);
+			n++;
+		}
+	}
+	else
+	{
+		n = i / 2;
+		while (str[n] != '\0')
+		{
+			_putchar(str[n]);
+			n++;
+		}
+	}
+}
