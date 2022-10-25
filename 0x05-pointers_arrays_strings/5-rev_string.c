c7b672cd42b89490ac6e4e7c3b1f242c76de7e2c
mwaskagi <gituiku.mwangi@gmail.com>
2022-10-25 12:55:16 -0700

    Reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - printcharcters in reverse
+ * @s: contain string
+ * Return: 0
+ */
+
+void rev_string(char * s)
+{
+	int a, b, c;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+	}
+
+	a--;
+
+	for (b = 0; a > b; b++)
+	{
+		c = s[a];
+		s[a--] = s[b];
+		s[b++] = c;
+	}
+}
