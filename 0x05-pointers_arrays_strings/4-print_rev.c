debe56b001cb0b7bcc2be548af241581dce64674
mwaskagi <gituiku.mwangi@gmail.com>
2022-10-25 12:42:57 -0700

    Print in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,13 +11,13 @@ void print_rev(char *s)
 	int count;
 
 	count = 0;
-	while (s[count] != s['\0'])
+	while (s[count] != '\0')
 	{
 		count++;
 	}
 
 	count--;
-	while (count != 0)
+	while (count >= 0)
 	{
 		_putchar(s[count]);
 		count--;
