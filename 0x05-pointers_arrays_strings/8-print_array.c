3daf8fa5ff47956fce2dedd0371b664f44b23682
mwaskagi <gituiku.mwangi@gmail.com>
2022-10-26 05:03:04 -0700

    Print Array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,15 +12,15 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	 if (n > 0)
-	 {
-		 for (i = 0; n > i; i++)
-		 {
-			 if (i != n -1)
-				 printf("%d, ", a[i]);
-			 else
-				 printf("%d", a[i]);
-		 }
-	 }
-	 printf("\n");
+	if (n > 0)
+	{
+		for (i = 0; n > i; i++)
+		{
+			if (i != n - 1)
+				printf("%d, ", a[i]);
+			else
+				printf("%d", a[i]);
+		}
+	}
+	printf("\n");
 }
