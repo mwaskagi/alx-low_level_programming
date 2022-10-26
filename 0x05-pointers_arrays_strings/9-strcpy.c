d1d34ff393831409bd88a78a17af32b26ddb2960
mwaskagi <gituiku.mwangi@gmail.com>
2022-10-26 05:15:30 -0700

    Copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string pointed to
+ * @dest: Array
+ * @src: source
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int a = 0;
+
+	while (src[a] != '\0')
+	{
+		dest[a] = src[a];
+		a++;
+	}
+	return (dest);
+}
