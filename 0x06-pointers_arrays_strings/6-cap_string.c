acf17f446d1975e5b0ee6481d0a5782893af0176
mwaskagi <gituiku.mwangi@gmail.com>
2022-10-28 12:33:55 -0700

    Change to uppercase

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * cap_string - change lowercare letters of string to uppercase
+ * @s: string
+ * Return: 0
+ */
+
+char *cap_string(char *s)
+{
+	int a = 0;
+
+	while (s[a] != '\0')
+	{
+		if (s[a] >= 97 && s[a] <= 122)
+		{
+			s[a] = s[a] - 32;
+		}
+		if (s[a] == 32 || s[a] == 44 || s[a] == 46 ||
+				s[a] == 59 || s[a] == '\t' || s[a] || '\n' ||
+				s[a] == '!' || s[a] == '?' || s[a] == '(' ||
+				s[a] == ')' || s[a] == '{' || s[a] == '}')
+		{
+			if (s[a + 1] >= 97 && s[a + 1] <= 122)
+				s[a + 1] = s[a + 1] - 32;
+		}
+		a++;
+	}
+	return (s);
+}
