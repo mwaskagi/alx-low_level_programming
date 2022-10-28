6a96518ff1d5f31e79414e5e16a6908c13c34076
mwaskagi <gituiku.mwangi@gmail.com>
2022-10-28 12:07:40 -0700

    Converts to Uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes lower letter to capital
+ * @s: string
+ * Return: 0
+ */
+
+char *string_toupper(char *s)
+{
+	int a = 0;
+
+	while (s[a] != '\0')
+	{
+		if((s[a] >= 97) && (s[a] <= 122))
+		{
+			s[a] = s[a] - 32;
+		}
+		a++;
+	}
+	return (s);
+	
+}
