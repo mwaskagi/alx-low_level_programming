e2590325edf6080527a7d1a4449b9390d9b90c77
mwaskagi <gituiku.mwangi@gmail.com>
2022-11-01 13:33:39 -0700

    Array multiplication

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "mian.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - print the sum of the two diagonals
+ * of a square marix of integers
+ * @a: array
+ * @size: size of array
+ * Return: 0
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int b, c, d = 0, e = 0;
+	
+	for (b = 0; b < size; b++)
+	{
+		for (c = 0; c < size; c++)
+		{
+			if (b == c)
+				d = d + a[(size + 1) * a];
+			if (b + c == size -1)
+				e = e + a[(size - 1) * (b + 1)];
+		}
+	}
+	printf("%i, %i\n", d, e);
+}
