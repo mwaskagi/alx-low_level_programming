df2e9fd4beb5bd3da2058ab9bd0907da8c62caed
mwaskagi <gituiku.mwangi@gmail.com>
2022-11-01 12:59:02 -0700

    Chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard - print chess board
+ * @a: array
+ * Return: 0
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int a, b;
+
+	for (a = 0; a < 8; a++)
+	{
+		for (b = 0; b < 8; b++)
+		{
+			_putchar(a[a][b]);
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
