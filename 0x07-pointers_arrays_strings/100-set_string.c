a22f27da71a9eef5eb4233f24db54d4dc8e69a31
mwaskagi <gituiku.mwangi@gmail.com>
2022-11-01 14:07:53 -0700

    Pointer to Value

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - set value of point to char
+ * @s: pointer to pointer
+ * @to: pointer
+ * Return: 0
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
