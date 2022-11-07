f6872549be0500c3f023060b9f8e7f9a54d076e5
mwaskagi <gituiku.mwangi@gmail.com>
2022-11-07 07:28:32 -0800

    My Name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print the name of program
+ * @argc: integer
+ * @argv: string array
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	printf("%s\n", argv[i]);
+	return (0);
+}
