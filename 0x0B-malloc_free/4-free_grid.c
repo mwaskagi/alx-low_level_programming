ea3442bf37e74f1fa0369de4a09440e10a9219b1
mwaskagi <gituiku.mwangi@gmail.com>
2022-11-09 11:31:03 -0800

    Free Up memory

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - frees a 2 dinebsional grid
+ * @grid: array
+ * @height: array
+ * Return: 0
+ */
+
+void free_grid(int **grid, int height)
+{
+	int a;
+
+	for (a = 0; a < height, a++)
+	{
+		free(*(grid + a));
+	}
+	free(grid);
+}
