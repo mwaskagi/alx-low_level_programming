ee7b49e6c443477aebf257d653fb9490fca15198
mwaskagi <gituiku.mwangi@gmail.com>
2022-11-15 08:37:04 -0800

    TYPEDEF

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - data type of dog
+ * @name: name of the dog
+ * @age: age of the dpg
+ * @owner: dog owner
+ * Return: 0
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
