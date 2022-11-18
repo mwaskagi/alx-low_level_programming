f3b5ee95f13aea87513c68a9fc4f7bb265d688d6
mwaskagi <gituiku.mwangi@gmail.com>
2022-11-18 07:50:51 -0800

    Print name using pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include  "function_pointers.h"
+
+/**
+ * print_name - prints name
+ * @name: name string
+ * @f: pointer
+ * Return: 0
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
