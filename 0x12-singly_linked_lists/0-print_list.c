f1471cf58ca823be96678687d8dd953c9ea4ebf6
mwaskagi <gituiku.mwangi@gmail.com>
2022-11-28 09:08:30 -0800

    Print List

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,9 +16,13 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (h->str == NULL)
+		{
 			printf("[0] (nill)\n");
+		}
 		else
+		{
 			printf("[%d] %s\n", h->len, h->str);
+		}
 		count++;
 		h = h->next;
 	}
