820e56055c6f102991bef85bbe92990d4b92c8c3
mwaskagi <gituiku.mwangi@gmail.com>
2022-12-05 11:44:12 -0800

    Clear the value of a bit

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+#include <math.h>
+
+/**
+ * clear_bit - Function that sets the value of bit to 
+ * 0 at given index
+ * @n: number
+ * @index: index
+ * Return: 1
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	int a;
+
+	if (index > 63)
+		return (-1);
+	a = 1 << index;
+	*n = *n & (~a);
+	return (1);
+}
